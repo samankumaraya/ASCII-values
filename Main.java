117b8f95dc772791c6321cbcc46333644777d0d5
Saman Kumara <94196963+samankumaraya@users.noreply.github.com>
2022-03-28 17:15:12 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package codin;
+
+import java.util.Scanner;
+
+public class Main {
+
+   public static void main(String args[]){
+
+       for(int i=0;i<=255;i++){
+           System.out.println("The ascii value of "+(char)i+" = "+i);
+       }
+   }
+    }
